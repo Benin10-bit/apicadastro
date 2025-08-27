2bc5f8bb61794cd3bf4746901fbf0a4d34c5f6b4
Benin10-bit <beniciodoamaralneto2019@gmail.com>
2025-08-26 21:12:56 -0300

    removendo o HTTPS

--- server.js
+++ server.js
@@ -1,21 +1,9 @@
 import Fastify from "fastify";
-import fs from "fs";
 import bcrypt from "bcrypt";
 import { dataBaseMemory } from "./database-memory.js";
 
-const server = Fastify({ 
+const server = Fastify({
   logger: true,
-  https: {
-    key: fs.readFileSync('./localhost+2-key.pem'),
-    cert: fs.readFileSync('./localhost+2.pem'),
-    allowHTTP1: true,
-    minVersion: 'TLSv1.2'
-  }
-});
-
-server.addHook('onSend', (request, reply, payload, done) => {
-  reply.header('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
-  done();
 });
 
 const dataBase = new dataBaseMemory();
@@ -24,17 +12,17 @@ server.post("/register", async (request, reply) => {
   const { nome, senha } = request.body;
   if (!nome || !senha)
     return reply.status(400).send({ error: "faltam campos" });
-  
+
   const hash = await bcrypt.hash(senha, 10);
   await dataBase.Create({ username: nome, password: hash });
-  
+
   return reply.status(201).send();
 });
 
 // Para listar usuários (somente teste)
-server.get('/users', () => {
+server.get("/users", () => {
   const users = dataBase.List();
   return users;
 });
 
-server.listen({ port: 1992, host: '0.0.0.0' });
+server.listen({ port: 1992, host: "0.0.0.0" });
