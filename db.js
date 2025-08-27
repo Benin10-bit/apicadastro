28723fc9bce6d6a87e1cfd24d515d8e878943331
Benin10-bit <beniciodoamaralneto2019@gmail.com>
2025-08-26 21:03:17 -0300

    sla

--- db.js
+++ db.js
@@ -2,5 +2,5 @@ import "dotenv/config";
 import http from "http";
 import { neon } from "@neondatabase/serverless";
 
-export const sql = neon(process.env.DATABASE_URL);
+export const sql = neon('postgresql://neondb_owner:npg_Wbhj2oPiAM3a@ep-cold-voice-ad4dutrq-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require');
 
